633a25a6c36a4868fa9af06d1c972c5e75d0d50a
David McArthur <david.mcarthur72@outlook.com>
2019-12-05 10:26:50 -0800

    Add project files.

---
+++ MSSAJobBoardLoginCapable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSSAJobBoardLoginCapable.Startup))]
+namespace MSSAJobBoardLoginCapable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
